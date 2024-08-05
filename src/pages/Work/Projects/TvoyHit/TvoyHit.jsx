6b0e7cdf66d48aa6e98e67fce5dac534244d1a33
abrosxd <154523512+abrosxd@users.noreply.github.com>
2024-08-05 13:02:07 +0200

    Upd

---
+++ src/pages/Work/Projects/TvoyHit/TvoyHit.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./TvoyHit.css";
+
+export default function TvoyHit() {
+  return (
+    <div className="tvoy-hit">
+      <div className="header">
+        <img src="/public/assets/work/projects/TvoyHit/header.png" />
+      </div>
+    </div>
+  );
+}
--- src/pages/Work/Work.jsx
+++ src/pages/Work/Work.jsx
@@ -5,21 +5,25 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import { Thumbs, Keyboard } from "swiper/modules";
 import "swiper/css";
 
+import TvoyHit from "./Projects/TvoyHit/TvoyHit";
+
 const Work = () => {
   const [thumbsSwiper, setThumbsSwiper] = useState(null);
   const items = Array.from({ length: 50 }, (_, i) => `Item ${i + 1}`);
+  const projects = [<TvoyHit />, <TvoyHit />];
 
   return (
     <main className="work">
       <div className="projects">
         <Swiper
+          className="window"
           onSwiper={setThumbsSwiper}
           spaceBetween={35}
           slidesPerView={1}
           modules={[Thumbs]}
           allowTouchMove={false}
         >
-          {items.map((item, index) => (
+          {projects.map((item, index) => (
             <SwiperSlide key={index}>
               <div className="card">{item}</div>
             </SwiperSlide>
@@ -30,13 +34,21 @@ const Work = () => {
         <div className="frame"></div>
         <Swiper
           className="slider"
-          spaceBetween={34}
-          slidesPerView={14}
+          spaceBetween={18}
+          slidesPerView={16}
           centeredSlides={true}
           modules={[Thumbs, Keyboard]}
           thumbs={{ swiper: thumbsSwiper }}
           keyboard={{ enabled: true }}
           slideToClickedSlide={true}
+          breakpoints={{
+            0: {
+              spaceBetween: 24,
+            },
+            768: {
+              spaceBetween: 14,
+            },
+          }}
         >
           {items.map((item, index) => (
             <SwiperSlide key={index}>
