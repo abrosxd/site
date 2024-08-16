9cee0e0618fd87fe17bfac7ca0b8a96195e31783
abrosxd <154523512+abrosxd@users.noreply.github.com>
2024-08-16 23:22:22 +0200

    Upd

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -7,20 +7,49 @@ import Transition from "../../components/Transition/Transition";
 import Button from "../../partials/Button/Button";
 import CrossSVG from "./assets/cross.svg";
 
+const SocialLinks = ({ title, links }) => (
+  <div className="block links">
+    <h2>{title}</h2>
+    {links.map(({ href, imgSrc, name }, index) => (
+      <a key={index} href={href} target="_blank" rel="noopener noreferrer">
+        <img src={imgSrc} alt={name} />
+        <p>{name}</p>
+      </a>
+    ))}
+  </div>
+);
+
+const StackItems = ({ items }) => (
+  <div className="block stack">
+    <h1>{items.title}</h1>
+    <div className="items">
+      {items.stacks.map(({ imgSrc, name }, index) => (
+        <div
+          key={index}
+          className={`tag ${name.toLowerCase().replace(/[\s.]/g, "-")}`}
+        >
+          <img src={imgSrc} alt={name} />
+          <span>{name}</span>
+        </div>
+      ))}
+    </div>
+  </div>
+);
+
 const About = () => {
   const { t } = useTranslation("About");
 
-  const gallery = [
-    "/assets/photo/gallery/1.jpeg",
-    "/assets/photo/gallery/2.jpeg",
-    "/assets/photo/gallery/3.png",
-    "/assets/photo/gallery/4.png",
-    "/assets/photo/gallery/5.png",
-  ];
+  const gallery = Array.from(
+    { length: 45 },
+    (_, index) => `/assets/photo/gallery/${index + 1}.jpeg`
+  );
 
-  const [currentDay, setCurrentDay] = useState("");
-  const [currentDate, setCurrentDate] = useState("");
-  const [currentTime, setCurrentTime] = useState("");
+  const [currentDateTime, setCurrentDateTime] = useState({
+    day: "",
+    date: "",
+    time: "",
+  });
+
   const [expandedSection, setExpandedSection] = useState(null);
 
   const updateDateTime = () => {
@@ -28,19 +57,15 @@ const About = () => {
       new Date().toLocaleString("en-US", { timeZone: "Europe/Warsaw" })
     );
     const daysOfWeek = t("info.time.days", { returnObjects: true });
-    const currentDay = daysOfWeek[nowInWarsaw.getDay()];
-    const currentDate = `${nowInWarsaw
-      .getDate()
-      .toString()
-      .padStart(2, "0")}/${(nowInWarsaw.getMonth() + 1)
+    const day = daysOfWeek[nowInWarsaw.getDay()];
+    const date = `${nowInWarsaw.getDate().toString().padStart(2, "0")}/${(
+      nowInWarsaw.getMonth() + 1
+    )
       .toString()
       .padStart(2, "0")}`;
-    const currentTime = nowInWarsaw.toLocaleTimeString("en-US", {
-      hour12: false,
-    });
-    setCurrentDay(currentDay);
-    setCurrentDate(currentDate);
-    setCurrentTime(currentTime);
+    const time = nowInWarsaw.toLocaleTimeString("en-US", { hour12: false });
+
+    setCurrentDateTime({ day, date, time });
   };
 
   useEffect(() => {
@@ -50,13 +75,52 @@ const About = () => {
   }, []);
 
   const handleExpandSection = (section) => {
-    if (window.innerWidth <= 960) {
-      setExpandedSection(section);
-    }
+    if (window.innerWidth <= 960) setExpandedSection(section);
   };
 
-  const closeSection = () => {
-    setExpandedSection(null);
+  const closeSection = () => setExpandedSection(null);
+
+  const socialLinks = [
+    {
+      href: "https://t.me/abrosxd",
+      imgSrc: "/public/assets/icons/social/telegram.svg",
+      name: "Telegram",
+    },
+    {
+      href: "https://www.facebook.com/abrosxd/",
+      imgSrc: "/public/assets/icons/social/facebook.svg",
+      name: "Facebook",
+    },
+    {
+      href: "https://vk.com/abrosxd",
+      imgSrc: "/public/assets/icons/social/vk.svg",
+      name: "Вконтакте",
+    },
+  ];
+
+  const resourceLinks = [
+    {
+      href: "https://codepen.io/abros",
+      imgSrc: "/public/assets/icons/social/codepen.svg",
+      name: "Codepen",
+    },
+  ];
+
+  const stackItems = {
+    title: t("info.stack"),
+    stacks: [
+      { imgSrc: "/assets/icons/stack/vs-code.jpg", name: "VS Code" },
+      { imgSrc: "/assets/icons/stack/html5.png", name: "HTML" },
+      { imgSrc: "/assets/icons/stack/css3.png", name: "CSS" },
+      { imgSrc: "/assets/icons/stack/js.png", name: "JavaScript" },
+      { imgSrc: "/assets/icons/stack/react.png", name: "React" },
+      { imgSrc: "/assets/icons/stack/gsap.png", name: "GSAP" },
+      { imgSrc: "/assets/icons/stack/node-js.svg", name: "Node.js" },
+      { imgSrc: "/assets/icons/stack/figma.png", name: "Figma" },
+      { imgSrc: "/assets/icons/stack/webflow.png", name: "Webflow" },
+      { imgSrc: "/assets/icons/stack/tilda.png", name: "Tilda" },
+      { imgSrc: "/assets/icons/stack/airtable.svg", name: "Airtable" },
+    ],
   };
 
   return (
@@ -65,32 +129,21 @@ const About = () => {
         className={`left ${expandedSection === "left" ? "expanded" : ""}`}
         onClick={() => handleExpandSection("left")}
       >
-        <img className="bg" src="/assets/photo/mona-lisa.png" />
+        <img className="bg" src="/assets/photo/statue1.avif" alt="Statue 1" />
         <div className="overlay">
           <h2>{t("links")}</h2>
         </div>
         <div className="content">
-          <div className="block links">
-            <h2>Связь со мной</h2>
-            <a href="https://t.me/abrosxd" target="_blank">
-              <img src="/public/assets/icons/social/telegram.svg" />
-              <p>Telegram</p>
-            </a>
-          </div>
-          <div className="block links">
-            <h2>Ресурсы</h2>
-            <a href="https://codepen.io/abros" target="_blank">
-              <img src="/public/assets/icons/social/codepen.svg" />
-              <p>Codepen</p>
-            </a>
-          </div>
+          <SocialLinks title="Связь со мной" links={socialLinks} />
+          <SocialLinks title="Ресурсы" links={resourceLinks} />
         </div>
       </div>
+
       <div
         className={`center ${expandedSection === "center" ? "expanded" : ""}`}
         onClick={() => handleExpandSection("center")}
       >
-        <img className="bg" src="/assets/photo/statue.png" />
+        <img className="bg" src="/assets/photo/statue2.png" alt="Statue 2" />
         <div className="overlay">
           <h2>{t("info.title")}</h2>
         </div>
@@ -105,110 +158,26 @@ const About = () => {
             </div>
             <div className="dayblock">
               <div className="date">
-                <p className="day">{currentDay}</p>
-                <p>{currentDate}</p>
+                <p className="day">{currentDateTime.day}</p>
+                <p>{currentDateTime.date}</p>
               </div>
               <div className="timeblock">
-                <span>{currentTime}</span>
+                <span>{currentDateTime.time}</span>
               </div>
             </div>
           </div>
-          <div className="block stack">
-            <h1>{t("info.stack")}</h1>
-            <div className="items">
-              <div className="tag vs-code">
-                <img src="/assets/icons/stack/vs-code.jpg" alt="VS Code" />
-                <span>VS Code</span>
-              </div>
-              <div className="tag html">
-                <img src="/assets/icons/stack/html5.png" alt="HTML" />
-                <span>HTML</span>
-              </div>
-              <div className="tag css">
-                <img src="/assets/icons/stack/css3.png" alt="CSS" />
-                <span>CSS</span>
-              </div>
-              <div className="tag js">
-                <img src="/assets/icons/stack/js.png" alt="JavaScript" />
-                <span>JavaScript</span>
-              </div>
-              <div className="tag react">
-                <img src="/assets/icons/stack/react.png" alt="React" />
-                <span>React</span>
-              </div>
-              <div className="tag gsap">
-                <img src="/assets/icons/stack/gsap.png" alt="GSAP" />
-                <span>GSAP</span>
-              </div>
-              <div className="tag node-js">
-                <img src="/assets/icons/stack/node-js.svg" alt="Node.js" />
-                <span>Node.js</span>
-              </div>
-              <div className="tag figma">
-                <img src="/assets/icons/stack/figma.png" alt="Figma" />
-                <span>Figma</span>
-              </div>
-              <div className="tag wordpress">
-                <img
-                  src="/public/assets/icons/stack/wordpress.png"
-                  alt="WordPress"
-                />
-                <span>WordPress</span>
-              </div>
-              <div className="tag webflow">
-                <img src="/assets/icons/stack/webflow.png" alt="Webflow" />
-                <span>Webflow</span>
-              </div>
-              <div className="tag tilda">
-                <img src="/assets/icons/stack/tilda.png" alt="Tilda" />
-                <span>Tilda</span>
-              </div>
-              <div className="tag airtable">
-                <img src="/assets/icons/stack/airtable.svg" alt="Airtable" />
-                <span>Airtable</span>
-              </div>
-              <div className="tag make">
-                <img src="/public/assets/icons/stack/make.png" alt="Make" />
-                <span>Make</span>
-              </div>
-              <div className="tag zapier">
-                <img src="/public/assets/icons/stack/zapier.png" alt="Zapier" />
-                <span>Zapier</span>
-              </div>
-              <div className="tag n8n">
-                <img src="/public/assets/icons/stack/n8n.svg" alt="N8N" />
-                <span>N8N</span>
-              </div>
-              <div className="tag nocodb">
-                <img src="/assets/icons/stack/nocodb.png" alt="NocoDB" />
-                <span>NocoDB</span>
-              </div>
-            </div>
-          </div>
+          <StackItems items={stackItems} />
           <div className="block desc">
-            <h1>Кто я.</h1>
-            <h3>
-              Меня зовут Daniel Abros, и я человек. Давайте будем реалистами,
-              можете ли вы действительно охарактеризовать человека с помощью
-              пары забавных фактов?
-            </h3>
-            <h1>Что я делаю.</h1>
-            <h3>
-              Я работаю frontend разработчиком-фрилансером на полную ставку.
-              Хотя я специализируюсь на создании веб-сайтов, я люблю
-              разнообразие и люблю браться за уникальные творческие задачи. Если
-              вы хотите изменить мир или переосмыслить мир, в котором мы живем,
-              я бы с удовольствием стал его частью.
-            </h3>
-            <h1>Откуда я родом.</h1>
-            <h3>
-              Я вырос в сибирском городе Сургут (если вы не знаете, где это, вам
-              определенно стоит поискать). На данный момент я живу в прекрасной
-              столице Польши - Варшава.
-            </h3>
+            <h1>{t("info.desc.me.title")}</h1>
+            <h3>{t("info.desc.me.text")}</h3>
+            <h1>{t("info.desc.doing.title")}</h1>
+            <h3>{t("info.desc.doing.text")}</h3>
+            <h1>{t("info.desc.from.title")}</h1>
+            <h3>{t("info.desc.from.text")}</h3>
           </div>
         </div>
       </div>
+
       <div className="right">
         <div className="overlay">
           <h2>{t("gallery")}</h2>
@@ -218,13 +187,6 @@ const About = () => {
           grabCursor={true}
           centeredSlides={false}
           slidesPerView={2}
-          coverflowEffect={{
-            rotate: 50,
-            stretch: 0,
-            depth: 100,
-            modifier: 1,
-            slideShadows: true,
-          }}
           autoplay={{
             delay: 2500,
             disableOnInteraction: false,
@@ -234,9 +196,7 @@ const About = () => {
           slidesPerGroup={1}
           className="gallery"
           breakpoints={{
-            960: {
-              direction: "vertical",
-            },
+            960: { direction: "vertical" },
           }}
         >
           {gallery.map((image, index) => (
@@ -246,6 +206,7 @@ const About = () => {
           ))}
         </Swiper>
       </div>
+
       {expandedSection && (
         <Button className="close" onClick={closeSection}>
           <img className="cross-icon" src={CrossSVG} alt="Close icon" />
